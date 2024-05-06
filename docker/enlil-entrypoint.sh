1535dc81527a80c833e6385c37f3c2cb084bc78f
Chris Penner <christopher.penner@gmail.com>
2024-05-06 11:05:35 -0700

    Rewrite docker enlil -> share

--- docker/enlil-entrypoint.sh
+++ docker/enlil-entrypoint.sh
@@ -2,21 +2,21 @@
 
 set -ex
 
-echo ENLIL_REDIS: "$ENLIL_REDIS"
+echo SHARE_REDIS: "$SHARE_REDIS"
 
-if [ -n "$NOMAD_PORT_enlil_tcp" ]; then
-    export ENLIL_CLIENT_PORT="$NOMAD_PORT_enlil_tcp"
+if [ -n "$NOMAD_PORT_share_tcp" ]; then
+    export SHARE_CLIENT_PORT="$NOMAD_PORT_share_tcp"
 fi
 
-if [ -n "$NOMAD_IP_enlil_tcp" ]; then
-    export ENLIL_CLIENT_HOSTNAME="$NOMAD_IP_enlil_tcp"
+if [ -n "$NOMAD_IP_share_tcp" ]; then
+    export SHARE_CLIENT_HOSTNAME="$NOMAD_IP_share_tcp"
 fi
 
-if [ -n "$NOMAD_PORT_enlil_http" ]; then
-    export ENLIL_SERVER_PORT="$NOMAD_PORT_enlil_http"
+if [ -n "$NOMAD_PORT_share_http" ]; then
+    export SHARE_SERVER_PORT="$NOMAD_PORT_share_http"
 fi
 
-export ENLIL_IP=0.0.0.0
+export SHARE_IP=0.0.0.0
 
 exec 2>&1
-exec /usr/local/bin/enlil
+exec /usr/local/bin/share
