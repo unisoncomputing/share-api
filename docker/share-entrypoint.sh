63748ceacf65d362a128ab03fd3b312faf55c580
Chris Penner <christopher.penner@gmail.com>
2024-05-10 12:13:30 -0700

    Fix NOMAD environment vars

--- docker/share-entrypoint.sh
+++ docker/share-entrypoint.sh
@@ -4,16 +4,16 @@ set -ex
 
 echo SHARE_REDIS: "$SHARE_REDIS"
 
-if [ -n "$NOMAD_PORT_share_tcp" ]; then
-    export SHARE_CLIENT_PORT="$NOMAD_PORT_share_tcp"
+if [ -n "$NOMAD_PORT_enlil_tcp" ]; then
+    export SHARE_CLIENT_PORT="$NOMAD_PORT_enlil_tcp"
 fi
 
-if [ -n "$NOMAD_IP_share_tcp" ]; then
-    export SHARE_CLIENT_HOSTNAME="$NOMAD_IP_share_tcp"
+if [ -n "$NOMAD_IP_enlil_tcp" ]; then
+    export SHARE_CLIENT_HOSTNAME="$NOMAD_IP_enlil_tcp"
 fi
 
-if [ -n "$NOMAD_PORT_share_http" ]; then
-    export SHARE_SERVER_PORT="$NOMAD_PORT_share_http"
+if [ -n "$NOMAD_PORT_enlil_http" ]; then
+    export SHARE_SERVER_PORT="$NOMAD_PORT_enlil_http"
 fi
 
 export SHARE_IP=0.0.0.0
