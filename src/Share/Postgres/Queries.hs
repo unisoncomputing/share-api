79bc5e3be8d84af1e966bebeac0bcffe45b9119c
Chris Penner <christopher.penner@gmail.com>
2024-09-04 11:00:21 -0700

    Fix project slug prefix search

--- src/Share/Postgres/Queries.hs
+++ src/Share/Postgres/Queries.hs
@@ -292,26 +292,24 @@ searchProjects caller (Just userId) (Query "") limit = do
       |]
     <&> fmap \(project PG.:. PG.Only handle) -> (project, handle)
 searchProjects caller userIdFilter (Query query) limit = do
-  let prefixQuery =
+  let queryToken =
         query
           -- Remove any chars with special meaning for tsqueries.
           & Text.filter (\c -> Char.isAlphaNum c || Char.isSpace c || c `elem` ['_', '-'])
           & \case
             -- Empty prefix searches are invalid, Nullify the prefix search.
             "" -> Nothing
-            txt ->
-              -- Convert into a quoted prefix query expression.
-              Just $ "'" <> txt <> "':*"
+            txt -> Just txt
   results <-
     PG.queryListRows
       [PG.sql|
     SELECT p.id, p.owner_user_id, p.slug, p.summary, p.tags, p.private, p.created_at, p.updated_at, owner.handle
-      FROM websearch_to_tsquery('english', #{query}) AS webquery, to_tsquery('english', #{prefixQuery}) AS prefixquery, projects AS p
+      FROM to_tsquery('english', #{queryToken}) AS tokenquery, projects AS p
         JOIN users AS owner ON p.owner_user_id = owner.id
-      WHERE (webquery @@ p.project_text_document OR prefixquery @@ p.project_text_document)
+      WHERE (tokenquery @@ p.project_text_document OR p.slug ILIKE (like_escape(#{query}) || '%'))
       AND (NOT p.private OR (#{caller} IS NOT NULL AND EXISTS (SELECT FROM accessible_private_projects WHERE user_id = #{caller} AND project_id = p.id)))
       AND (#{userIdFilter} IS NULL OR p.owner_user_id = #{userIdFilter})
-      ORDER BY (ts_rank_cd(p.project_text_document, webquery), ts_rank_cd(p.project_text_document, prefixquery)) DESC
+      ORDER BY ts_rank_cd(p.project_text_document, tokenquery) DESC, p.slug ASC
       LIMIT #{limit}
       |]
   pure (results <&> \(project PG.:. PG.Only handle) -> (project, handle))
