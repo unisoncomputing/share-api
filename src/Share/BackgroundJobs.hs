e418855fec7b75cd0f5c7e3687848fcdedf7e2d9
Chris Penner <christopher.penner@gmail.com>
2025-01-24 13:02:05 -0800

    Disable Contribution Diffs background job for now

--- src/Share/BackgroundJobs.hs
+++ src/Share/BackgroundJobs.hs
@@ -1,7 +1,6 @@
 module Share.BackgroundJobs (startWorkers) where
 
 import Ki.Unlifted qualified as Ki
-import Share.BackgroundJobs.Diffs.ContributionDiffs qualified as ContributionDiffs
 import Share.BackgroundJobs.Monad (Background)
 import Share.BackgroundJobs.Search.DefinitionSync qualified as DefnSearch
 import Share.BackgroundJobs.SerializedEntitiesMigration.Worker qualified as SerializedEntitiesMigration
@@ -10,5 +9,6 @@ import Share.BackgroundJobs.SerializedEntitiesMigration.Worker qualified as Seri
 startWorkers :: Ki.Scope -> Background ()
 startWorkers scope = do
   DefnSearch.worker scope
-  ContributionDiffs.worker scope
+  -- Temporary disable background diff jobs until the new diffing logic is done.
+  -- ContributionDiffs.worker scope
   SerializedEntitiesMigration.worker scope
