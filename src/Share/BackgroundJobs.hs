bf940524537f4cf33941ed11965e13a191f65227
Chris Penner <christopher.penner@gmail.com>
2025-01-24 12:37:19 -0800

    Serialise -> Serialized

--- src/Share/BackgroundJobs.hs
+++ src/Share/BackgroundJobs.hs
@@ -4,11 +4,11 @@ import Ki.Unlifted qualified as Ki
 import Share.BackgroundJobs.Diffs.ContributionDiffs qualified as ContributionDiffs
 import Share.BackgroundJobs.Monad (Background)
 import Share.BackgroundJobs.Search.DefinitionSync qualified as DefnSearch
-import Share.BackgroundJobs.SerialisedEntitiesMigration.Worker qualified as SerialisedEntitiesMigration
+import Share.BackgroundJobs.SerializedEntitiesMigration.Worker qualified as SerializedEntitiesMigration
 
 -- | Kicks off all background workers.
 startWorkers :: Ki.Scope -> Background ()
 startWorkers scope = do
   DefnSearch.worker scope
   ContributionDiffs.worker scope
-  SerialisedEntitiesMigration.worker scope
+  SerializedEntitiesMigration.worker scope
---
+++ src/Share/BackgroundJobs/SerializedEntitiesMigration/Queries.hs
@@ -0,0 +1,22 @@
+module Share.BackgroundJobs.SerializedEntitiesMigration.Queries (claimEntity) where
+
+import Share.IDs
+import Share.Postgres
+import Unison.Hash32
+
+claimEntity :: Transaction e (Maybe (Hash32, UserId))
+claimEntity = do
+  query1Row
+    [sql|
+    WITH chosen_entity(hash, user_id) AS (
+      SELECT q.hash, q.user_id
+      FROM migrate_serialised_queue q
+      LIMIT 1
+      -- Skip any that are being synced by other workers.
+      FOR UPDATE SKIP LOCKED
+    )
+    DELETE FROM migrate_serialised_queue
+      USING chosen_entity
+      WHERE migrate_serialised_queue.hash = chosen_entity.hash AND migrate_serialised_queue.user_id = chosen_entity.user_id
+    RETURNING chosen_entity.hash, chosen_entity.user_id
+    |]
