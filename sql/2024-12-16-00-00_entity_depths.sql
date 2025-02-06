d7d617f273654e30d08a84fb0b5f2fe2d6b0446a
Chris Penner <christopher.penner@gmail.com>
2025-02-05 16:08:36 -0800

    Revive entity depth sort

---
+++ sql/2024-12-16-00-00_entity_depths.sql
@@ -0,0 +1,226 @@
+-- We can track the maximum dependency depth of any sub-dag rooted at each entity.
+-- The depth of any entity is simply the maximum depth of any of its children plus one.
+-- This allows us to trivially sort entities into a valid dependency order without needing a complex topological
+-- sort at query time.
+
+-- Unfortunately we can't use triggers for most of these since for some entities their depth is dependent on
+-- references which, due to foreign keys, must be inserted AFTER the entity itself, it must be run after all
+-- the entity's local references are inserted, but there's no way for us to trigger
+-- only when the LAST one of those is done, so we'd need to run this on every
+-- local reference insert, and remove the optimistic exit in the case where the row
+-- already exists, which is a big waste.
+--
+-- Instead we just run these functions manually after an entity's references are all inserted.
+
+CREATE TABLE causal_depth (
+  causal_id INTEGER PRIMARY KEY REFERENCES causals (id) ON DELETE CASCADE,
+  depth INTEGER NOT NULL
+);
+
+CREATE TABLE component_depth (
+  component_hash_id INTEGER PRIMARY KEY REFERENCES component_hashes (id) ON DELETE CASCADE,
+  depth INTEGER NOT NULL
+);
+
+CREATE TABLE namespace_depth (
+  namespace_hash_id INTEGER PRIMARY KEY REFERENCES branch_hashes (id) ON DELETE CASCADE,
+  depth INTEGER NOT NULL
+);
+
+CREATE TABLE patch_depth (
+  patch_id INTEGER PRIMARY KEY REFERENCES patches (id) ON DELETE CASCADE,
+  depth INTEGER NOT NULL
+);
+
+
+-- Triggers
+
+CREATE OR REPLACE FUNCTION update_causal_depth(the_causal_id integer) RETURNS VOID AS $$
+DECLARE
+  max_namespace_depth INTEGER;
+  max_child_causal_depth INTEGER;
+  the_namespace_hash_id INTEGER;
+BEGIN
+  -- If there's already a depth entry for this causal, we're done.
+  IF EXISTS (SELECT FROM causal_depth cd WHERE cd.causal_id = the_causal_id) THEN
+    RETURN;
+  END IF;
+
+  SELECT c.namespace_hash_id INTO the_namespace_hash_id
+    FROM causals c
+    WHERE c.id = the_causal_id;
+  -- Find the max depth of the associated namespace
+  -- Find the max depth of any child causal
+  -- Set the depth of this causal to the max of those two plus one
+  SELECT COALESCE(MAX(nd.depth), -1) INTO max_namespace_depth
+    FROM namespace_depth nd
+    WHERE nd.namespace_hash_id = the_namespace_hash_id;
+  SELECT COALESCE(MAX(cd.depth), -1) INTO max_child_causal_depth
+    FROM causal_depth cd
+      JOIN causal_ancestors ca ON cd.causal_id = ca.ancestor_id
+    WHERE ca.causal_id = the_causal_id;
+  INSERT INTO causal_depth (causal_id, depth)
+    VALUES (the_causal_id, GREATEST(max_namespace_depth, max_child_causal_depth) + 1);
+
+  RETURN;
+END;
+$$ LANGUAGE plpgsql;
+
+CREATE OR REPLACE FUNCTION update_component_depth(the_component_hash_id integer) RETURNS VOID AS $$
+DECLARE
+  max_referenced_component_depth INTEGER;
+BEGIN
+  RAISE NOTICE 'Updating component depth for %', the_component_hash_id;
+  -- If there's already a depth entry for this component, we're done.
+   IF EXISTS (SELECT FROM component_depth cd WHERE cd.component_hash_id = the_component_hash_id) THEN
+     RETURN;
+   END IF;
+  -- Find the max depth of any component referenced by this component
+  -- Set the depth of this component to that plus one
+  SELECT COALESCE(MAX(refs.depth), -1) INTO max_referenced_component_depth
+    FROM (
+      ( SELECT cd.depth AS depth
+        FROM terms t
+        JOIN term_local_component_references cr
+          ON cr.term_id = t.id
+        JOIN component_depth cd
+          ON cd.component_hash_id = cr.component_hash_id
+        WHERE t.component_hash_id = the_component_hash_id
+      ) UNION
+      ( SELECT cd.depth AS depth
+        FROM types t
+        JOIN type_local_component_references cr
+          ON cr.type_id = t.id
+        JOIN component_depth cd
+          ON cd.component_hash_id = cr.component_hash_id
+        WHERE t.component_hash_id = the_component_hash_id
+      )
+  ) AS refs;
+  INSERT INTO component_depth (component_hash_id, depth)
+    VALUES (the_component_hash_id, max_referenced_component_depth + 1);
+  RETURN;
+END;
+$$ LANGUAGE plpgsql;
+
+CREATE OR REPLACE FUNCTION update_namespace_depth(the_namespace_hash_id integer) RETURNS VOID AS $$
+DECLARE
+  max_child_causal_depth INTEGER;
+  max_patch_depth INTEGER;
+  max_referenced_component_depth INTEGER;
+BEGIN
+  -- If there's already a depth entry for this namespace, we're done.
+  IF EXISTS (SELECT FROM namespace_depth nd WHERE nd.namespace_hash_id = the_namespace_hash_id) THEN
+    RETURN;
+  END IF;
+  -- Find the max depth of any child causal
+  -- Find the max depth of any patch
+  -- Find the max depth of any component referenced by a term, type, or term metadata in this namespace
+  -- Set the depth of this namespace to the max of those plus one
+  SELECT COALESCE(MAX(cd.depth), -1) INTO max_child_causal_depth
+    FROM causal_depth cd
+      JOIN namespace_children nc ON cd.causal_id = nc.child_causal_id
+    WHERE nc.parent_namespace_hash_id = the_namespace_hash_id;
+  SELECT COALESCE(MAX(pd.depth), -1) INTO max_patch_depth
+    FROM patch_depth pd
+      JOIN namespace_patches np ON pd.patch_id = np.patch_id
+    WHERE np.namespace_hash_id = the_namespace_hash_id;
+  SELECT COALESCE(MAX(depth), -1) INTO max_referenced_component_depth
+    FROM (
+      -- direct term references
+      ( SELECT cd.depth AS depth
+        FROM component_depth cd
+        JOIN term_local_component_references cr
+          ON cd.component_hash_id = cr.component_hash_id
+        JOIN terms t
+          ON cr.term_id = t.id
+        JOIN namespace_terms nt
+          ON t.id = nt.term_id
+        WHERE nt.namespace_hash_id = the_namespace_hash_id
+      ) UNION
+      -- term metadata references
+      ( SELECT cd.depth AS depth
+        FROM component_depth cd
+        JOIN terms t
+          ON cd.component_hash_id = t.component_hash_id
+        JOIN namespace_term_metadata ntm
+          ON ntm.metadata_term_id = t.id
+        JOIN namespace_terms nt
+          ON ntm.named_term = nt.id
+        WHERE nt.namespace_hash_id = the_namespace_hash_id
+      ) UNION
+      -- direct constructor references
+      ( SELECT cd.depth AS depth
+        FROM component_depth cd
+        JOIN constructors c
+          ON cd.component_hash_id = c.constructor_type_component_hash_id
+        JOIN namespace_terms nt
+          ON c.id = nt.constructor_id
+        WHERE nt.namespace_hash_id = the_namespace_hash_id
+      ) UNION
+      -- direct type references
+      ( SELECT cd.depth AS depth
+        FROM component_depth cd
+        JOIN type_local_component_references cr
+          ON cd.component_hash_id = cr.component_hash_id
+        JOIN types t
+          ON cr.type_id = t.id
+        JOIN namespace_types nt
+          ON t.id = nt.type_id
+        WHERE nt.namespace_hash_id = the_namespace_hash_id
+      ) UNION
+      -- type metadata references
+      ( SELECT cd.depth AS depth
+        FROM component_depth cd
+        JOIN terms t
+          ON cd.component_hash_id = t.component_hash_id
+        JOIN namespace_type_metadata ntm
+          ON ntm.metadata_term_id = t.id
+        JOIN namespace_types nt
+          ON ntm.named_type = nt.id
+        WHERE nt.namespace_hash_id = the_namespace_hash_id
+      )
+  ) AS refs;
+  INSERT INTO namespace_depth (namespace_hash_id, depth)
+    VALUES (the_namespace_hash_id, GREATEST(max_child_causal_depth, max_patch_depth, max_referenced_component_depth) + 1);
+
+  RETURN;
+END;
+$$ LANGUAGE plpgsql;
+
+CREATE OR REPLACE FUNCTION update_patch_depth(the_patch_id integer) RETURNS VOID AS $$
+DECLARE
+  max_referenced_component_depth INTEGER;
+BEGIN
+  -- If there's already a depth entry for this patch, we're done.
+  IF EXISTS (SELECT FROM patch_depth pd WHERE pd.patch_id = the_patch_id) THEN
+    RETURN;
+  END IF;
+  -- Find the max depth of any term component referenced by a patch
+  -- Find the max depth of any type component referenced by a patch
+  -- Set the depth of this patch to that plus one
+
+  SELECT COALESCE(MAX(cd.depth), -1) INTO max_referenced_component_depth
+    FROM (
+      -- term references
+      ( SELECT from_term_component_hash_id AS component_hash_id
+        FROM patch_term_mappings
+        WHERE patch_id = the_patch_id
+      ) UNION
+      -- constructor mappings
+      ( SELECT from_constructor_component_hash_id AS component_hash_id
+        FROM patch_constructor_mappings
+        WHERE patch_id = the_patch_id
+      ) UNION
+      -- type references
+      ( SELECT from_type_component_hash_id AS component_hash_id
+        FROM patch_type_mappings
+        WHERE patch_id = the_patch_id
+      )
+    ) AS refs JOIN component_depth cd
+      ON cd.component_hash_id = refs.component_hash_id;
+  INSERT INTO patch_depth (patch_id, depth)
+    VALUES (the_patch_id, max_referenced_component_depth + 1);
+
+  RETURN;
+END;
+$$ LANGUAGE plpgsql;
--- src/Share/Postgres/Definitions/Queries.hs
+++ src/Share/Postgres/Definitions/Queries.hs
@@ -863,6 +863,10 @@ saveEncodedTermComponent componentHash maySerialized elements = do
             SELECT defn_mappings.term_id, defn_mappings.local_index, defn_mappings.component_hash_id
               FROM defn_mappings
         |]
+      execute_
+        [sql|
+          SELECT update_component_depth(#{componentHashId})
+        |]
       pure termIds
 
 saveTypeComponent :: ComponentHash -> Maybe TempEntity -> [(PgLocalIds, DeclFormat.Decl Symbol)] -> CodebaseM e ()
@@ -1013,6 +1017,10 @@ saveTypeComponent componentHash maySerialized elements = do
               FROM defn_mappings
         |]
       saveConstructors (zip (toList typeIds) elements)
+      execute_
+        [sql|
+          SELECT update_component_depth(#{componentHashId})
+        |]
       pure typeIds
 
 -- | Efficiently resolve all pg Ids across selected Local Ids.
