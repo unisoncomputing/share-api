dc2db00659969965c8401ffb039158f742247212
Chris Penner <christopher.penner@gmail.com>
2025-07-11 13:42:30 -0700

    Helper utils in Telemetry.hs

--- src/Share/Telemetry.hs
+++ src/Share/Telemetry.hs
@@ -1,13 +1,29 @@
-module Share.Telemetry (withSpan) where
+module Share.Telemetry
+  ( withSpan,
+    TracerT (..),
+    runTracerT,
+    AttributeMap,
+    Trace.toAttribute,
+  )
+where
 
+import Control.Monad.Reader (MonadReader (..), MonadTrans, ReaderT (..))
+import Data.HashMap.Lazy qualified as HM
+import Data.Map qualified as Map
 import Data.Text (Text)
 import OpenTelemetry.Trace qualified as Trace
+import OpenTelemetry.Trace.Monad (MonadTracer)
 import OpenTelemetry.Trace.Monad qualified as TraceM
+import Share.Env (HasTags)
+import Share.Env qualified as Env
 import UnliftIO
 
-withSpan :: (MonadUnliftIO m, TraceM.MonadTracer m) => Text -> m a -> m a
-withSpan name action = do
-  let spanAttributes = mempty
+type AttributeMap = HM.HashMap Text Trace.Attribute
+
+withSpan :: (MonadUnliftIO m, TraceM.MonadTracer m, HasTags ctx, MonadReader ctx m) => Text -> AttributeMap -> m a -> m a
+withSpan name spanTags action = do
+  tags <- ask >>= Env.getTags
+  let spanAttributes = spanTags <> HM.fromList (Map.toList (Trace.toAttribute <$> tags))
   let spanArguments =
         Trace.SpanArguments
           { kind = Trace.Server,
@@ -16,3 +32,15 @@ withSpan name action = do
             startTime = Nothing -- This will be set automatically
           }
   TraceM.inSpan name spanArguments $ action
+
+-- | Helper for adding MonadTracer.
+newtype TracerT m a = TracerT
+  { unTracerT :: ReaderT Trace.Tracer m a
+  }
+  deriving newtype (Functor, Applicative, Monad, MonadIO, MonadUnliftIO, MonadTrans)
+
+runTracerT :: Trace.Tracer -> TracerT m a -> m a
+runTracerT tracer (TracerT action) = runReaderT action tracer
+
+instance (Monad m) => MonadTracer (TracerT m) where
+  getTracer = TracerT $ ask
