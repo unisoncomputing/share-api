4dd537f9825cdd5d70809bf538fe3d9dfdf5b019
Chris Penner <christopher.penner@gmail.com>
2025-08-21 13:47:41 -0700

    Start re-tooling auth stuff for the auth client, it's hard

--- share-client/src/Share/Client.hs
+++ share-client/src/Share/Client.hs
@@ -1,7 +1,15 @@
-module Share.Client () where
+module Share.Client (createOrg) where
 
+import Data.Proxy (Proxy (..))
+import Servant.API
 import Servant.Client
-import Share.Web.API (API, api)
+import Share.Web.API (API, OrgsAPI, api)
+import Share.Web.Share.DisplayInfo.Types qualified as Orgs
+import Share.Web.Share.Orgs.API qualified as OrgsAPI
+import Share.Web.Share.Orgs.Types qualified as Orgs
 
-_x :: Client ClientM OrgAPI
-_x = client api
+orgsClient :: Client ClientM OrgsAPI
+orgsClient = client (Proxy :: Proxy OrgsAPI)
+
+createOrg :: _ -> Orgs.CreateOrgRequest -> ClientM Orgs.OrgDisplayInfo
+(createOrg :<|> OrgsAPI.ResourceRoutes {OrgsAPI.roles = OrgsAPI.OrgRolesRoutes {}, OrgsAPI.members = OrgsAPI.OrgMembersRoutes {}}) = orgsClient
