c0f12833659906ef2bb9cd1201c09a8f73077bdf
Chris Penner <christopher.penner@gmail.com>
2025-10-03 16:37:46 -0700

    Implement more of the server-side sync3

--- src/Share/Web/UCM/SyncCommon/Impl.hs
+++ src/Share/Web/UCM/SyncCommon/Impl.hs
@@ -1,4 +1,36 @@
-module Share.Web.UCM.SyncCommon.Impl (codebaseForBranchRef) where
+module Share.Web.UCM.SyncCommon.Impl
+  ( parseBranchRef,
+    codebaseForBranchRef,
+  )
+where
+
+import Control.Monad.Except (ExceptT (ExceptT))
+import Servant
+import Share.Codebase qualified as Codebase
+import Share.IDs (ProjectBranchShortHand (..), ProjectReleaseShortHand (..), ProjectShortHand (..))
+import Share.IDs qualified as IDs
+import Share.Postgres qualified as PG
+import Share.Postgres.Queries qualified as PGQ
+import Share.Postgres.Users.Queries qualified as UserQ
+import Share.Prelude
+import Share.Project (Project (..))
+import Share.User (User (..))
+import Share.Web.App
+import Share.Web.Authorization qualified as AuthZ
+import Share.Web.UCM.SyncCommon.Types
+import U.Codebase.Sqlite.Orphans ()
+import Unison.SyncV2.Types qualified as SyncV2
+
+parseBranchRef :: SyncV2.BranchRef -> Either Text (Either ProjectReleaseShortHand ProjectBranchShortHand)
+parseBranchRef (SyncV2.BranchRef branchRef) =
+  case parseRelease <|> parseBranch of
+    Just a -> Right a
+    Nothing -> Left $ "Invalid repo info: " <> branchRef
+  where
+    parseBranch :: Maybe (Either ProjectReleaseShortHand ProjectBranchShortHand)
+    parseBranch = fmap Right . eitherToMaybe $ IDs.fromText @ProjectBranchShortHand branchRef
+    parseRelease :: Maybe (Either ProjectReleaseShortHand ProjectBranchShortHand)
+    parseRelease = fmap Left . eitherToMaybe $ IDs.fromText @ProjectReleaseShortHand branchRef
 
 codebaseForBranchRef :: SyncV2.BranchRef -> (ExceptT CodebaseLoadingError WebApp Codebase.CodebaseEnv)
 codebaseForBranchRef branchRef = do
