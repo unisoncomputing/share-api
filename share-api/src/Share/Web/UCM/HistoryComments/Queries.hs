daa8646588ee48031b0e8a9da1f196eadf244a16
Chris Penner <christopher.penner@gmail.com>
2025-11-21 12:26:14 -0800

    Write most of the History Comment insert queries

--- share-api/src/Share/Web/UCM/HistoryComments/Queries.hs
+++ share-api/src/Share/Web/UCM/HistoryComments/Queries.hs
@@ -1,20 +1,28 @@
 {-# LANGUAGE RecordWildCards #-}
 
-module Share.Web.UCM.HistoryComments.Queries (fetchProjectBranchCommentsSince) where
+module Share.Web.UCM.HistoryComments.Queries
+  ( fetchProjectBranchCommentsSince,
+    insertHistoryComments,
+  )
+where
 
+import Control.Lens
+import Data.Containers.ListUtils (nubOrd)
 import Data.Time (UTCTime)
+import Data.Time.Clock.POSIX qualified as POSIX
 import Share.IDs
 import Share.Postgres qualified as PG
 import Share.Postgres.Cursors (PGCursor)
 import Share.Postgres.Cursors qualified as PG
 import Share.Postgres.IDs
 import Share.Prelude
+import Share.Web.Authorization (AuthZReceipt)
 import Unison.Hash32 (Hash32)
 import Unison.Server.HistoryComments.Types
 
-fetchProjectBranchCommentsSince :: ProjectId -> CausalId -> UTCTime -> PG.Transaction e (PGCursor HistoryCommentChunk)
-fetchProjectBranchCommentsSince projectId causalId sinceTime = do
-  PG.newRowCursor @(Bool, Maybe Text, Maybe Text, Maybe UTCTime, Maybe Bool, Maybe ByteString, Maybe Hash32, Maybe Text, Maybe UTCTime, Maybe ByteString, Maybe Hash32, Maybe Hash32)
+fetchProjectBranchCommentsSince :: AuthZReceipt -> ProjectId -> CausalId -> UTCTime -> PG.Transaction e (PGCursor HistoryCommentChunk)
+fetchProjectBranchCommentsSince !_authZ projectId causalId sinceTime = do
+  PG.newRowCursor @(Bool, Maybe Text, Maybe Text, Maybe Int64, Maybe Bool, Maybe ByteString, Maybe Hash32, Maybe Text, Maybe Int64, Maybe ByteString, Maybe Hash32, Maybe Hash32)
     "fetchProjectBranchCommentsSince"
     [PG.sql|
     WITH revisions(id, comment_id, subject, contents, created_at_ms, hidden, revision_hash, author_signature) AS (
@@ -42,13 +50,137 @@ fetchProjectBranchCommentsSince projectId causalId sinceTime = do
       -- the vast majority of the time we'll need them, it simplifies logic,
       -- and the client can just ignore them if they already have them.
       (SELECT DISTINCT ON (rev.comment_id)
-        false, (rev.subject, rev.content, rev.created_at, rev.is_hidden, rev.author_signature, rev.revision_hash), NULL
+        false, (rev.subject, rev.content, rev.created_at_ms, rev.is_hidden, rev.author_signature, rev.revision_hash), NULL
       FROM revisions rev
       )
     |]
     <&> fmap \case
-      (True, Nothing, Nothing, Nothing, Nothing, Nothing, Nothing, Just author, Just createdAt, Just authorThumbprint, Just causalHash, Just commentHash) ->
-        HistoryCommentChunk $ HistoryComment {..}
-      (False, Just subject, Just content, Just createdAt, Just isHidden, Just authorSignature, Just revisionHash, Nothing, Nothing, Nothing, Nothing, Nothing) ->
-        HistoryCommentRevisionChunk $ HistoryCommentRevision {..}
+      (True, Nothing, Nothing, Nothing, Nothing, Nothing, Nothing, Just author, Just createdAtMs, Just authorThumbprint, Just causalHash, Just commentHash) ->
+        let createdAt = millisToUTCTime createdAtMs
+         in HistoryCommentChunk $ HistoryComment {..}
+      (False, Just subject, Just content, Just createdAtMs, Just isHidden, Just authorSignature, Just revisionHash, Nothing, Nothing, Nothing, Nothing, Nothing) ->
+        let createdAt = millisToUTCTime createdAtMs
+         in HistoryCommentRevisionChunk $ HistoryCommentRevision {..}
       row -> error $ "fetchProjectBranchCommentsSince: Unexpected row format: " <> show row
+
+insertThumbprints :: (PG.QueryA m) => [Text] -> m ()
+insertThumbprints thumbprints = do
+  PG.execute_
+    [PG.sql|
+      INSERT INTO personal_keys (thumbprint)
+      SELECT * FROM ^{PG.toTable $ nubOrd thumbprints}
+      ON CONFLICT (thumbprint) DO NOTHING
+    |]
+
+-- Convert milliseconds since epoch to UTCTime _exactly_.
+-- UTCTime has picosecond precision so this is lossless.
+millisToUTCTime :: Int64 -> UTCTime
+millisToUTCTime ms =
+  toRational ms
+    & (/ (1_000 :: Rational))
+    & fromRational
+    & POSIX.posixSecondsToUTCTime
+
+utcTimeToMillis :: UTCTime -> Int64
+utcTimeToMillis utcTime =
+  POSIX.utcTimeToPOSIXSeconds utcTime
+    & toRational
+    & (* (1_000 :: Rational))
+    & round
+
+insertHistoryComments :: AuthZReceipt -> ProjectId -> [HistoryCommentChunk] -> PG.Transaction e ()
+insertHistoryComments !_authZ projectId chunks = PG.pipelined $ do
+  let (comments, revisions) =
+        chunks & foldMap \case
+          HistoryCommentChunk comment -> ([comment], [])
+          HistoryCommentRevisionChunk revision -> ([], [revision])
+          HistoryCommentErrorChunk err -> error $ "HistoryCommentErrorChunk: " <> show err -- TODO Handle this
+  insertThumbprints $ comments <&> \HistoryComment {authorThumbprint} -> authorThumbprint
+  insertHistoryComments comments
+  insertRevisions revisions
+  insertDiscoveryInfo revisions
+  where
+    insertHistoryComments :: [HistoryComment] -> PG.Pipeline e ()
+    insertHistoryComments comments = do
+      let commentsTable =
+            comments <&> \HistoryComment {..} ->
+              ( author,
+                utcTimeToMillis createdAt,
+                authorThumbprint,
+                causalHash,
+                commentHash
+              )
+      PG.execute_
+        [PG.sql|
+          WITH new_comments(author, created_at_ms, author_thumbprint, causal_hash, comment_hash) AS (
+            VALUES ^{PG.toTable commentsTable}
+          )
+          INSERT INTO history_comments(causal_id, author, created_at_ms, comment_hash, author_key_id)
+            SELECT causal.id, nc.author, nc.created_at_ms, nc.comment_hash, pk.id
+            FROM new_comments nc
+            JOIN causal causal
+              ON causal.hash = nc.causal_hash
+            JOIN personal_keys pk
+              ON pk.thumbprint = nc.author_thumbprint
+          ON CONFLICT DO NOTHING
+        |]
+
+    insertRevisions :: [HistoryCommentRevision] -> PG.Pipeline e ()
+    insertRevisions revs = do
+      let revsTable =
+            revs <&> \HistoryCommentRevision {..} ->
+              ( subject,
+                content,
+                utcTimeToMillis createdAt,
+                isHidden,
+                authorSignature,
+                revisionHash,
+                commentHash
+              )
+      PG.execute_
+        [PG.sql|
+          WITH new_revisions(subject, content, created_at_ms, hidden, author_signature, revision_hash, comment_hash) AS (
+            VALUES ^{PG.toTable revsTable}
+          )
+          INSERT INTO history_comment_revisions(comment_id, subject, contents, created_at_ms, hidden, author_signature, revision_hash)
+            SELECT hc.id, nr.subject, nr.contents, nr.created_at_ms, nr.hidden, nr.author_signature, nr.revision_hash
+            FROM new_revisions nr
+            JOIN history_comments hc
+              ON hc.comment_hash = nr.comment_hash
+          ON CONFLICT DO NOTHING
+        |]
+      let revHashTable = revs <&> \HistoryCommentRevision {..} -> (revisionHash)
+      PG.execute_
+        [PG.sql|
+          WITH new_discoveries(revision_hash) AS (
+            VALUES ^{PG.singleColumnTable revHashTable}
+          )
+          INSERT INTO history_comment_revisions_project_discovery(project_id, history_comment_revision_id)
+            SELECT #{projectId}, hcr.id
+            FROM new_discoveries nd
+            JOIN history_comments hcr
+              ON hcr.revision_hash = nd.revision_hash
+          ON CONFLICT DO NOTHING
+        |]
+    insertDiscoveryInfo :: [HistoryCommentRevision] -> PG.Pipeline e ()
+    insertDiscoveryInfo revs = do
+      let discoveryTable =
+            revs <&> \HistoryCommentRevision {..} ->
+              ( projectId,
+                commentHash,
+                utcTimeToMillis createdAt
+              )
+      PG.execute_
+        [PG.sql|
+          WITH new_discoveries(project_id, history_comment_hash, discovered_at) AS (
+            VALUES ^{PG.toTable discoveryTable}
+          )
+          INSERT INTO history_comment_revisions_project_discovery(project_id, history_comment_revision_id, discovered_at)
+            SELECT #{projectId}, hcr.id, nd.discovered_at
+            FROM new_discoveries nd
+            JOIN history_comments hc
+              ON hc.comment_hash = nd.history_comment_hash
+            JOIN history_comment_revisions hcr
+              ON hcr.history_comment_id = hc.id
+          ON CONFLICT DO NOTHING
+        |]
