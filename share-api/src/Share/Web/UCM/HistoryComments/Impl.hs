f596a865039cfa6979aa9842c813c71eb2bf169d
Chris Penner <christopher.penner@gmail.com>
2025-11-26 13:11:28 -0800

    Integrate new Queuing primitives

--- share-api/src/Share/Web/UCM/HistoryComments/Impl.hs
+++ share-api/src/Share/Web/UCM/HistoryComments/Impl.hs
@@ -2,6 +2,7 @@ module Share.Web.UCM.HistoryComments.Impl (server) where
 
 import Conduit (ConduitT)
 import Data.Void
+import Network.WebSockets.Connection
 import Servant
 import Share.IDs
 import Share.Utils.Servant.Streaming qualified as Streaming
@@ -10,6 +11,7 @@ import Share.Web.Errors (Unimplemented (Unimplemented), respondError)
 import Unison.Server.HistoryComments.API qualified as HistoryComments
 import Unison.Server.HistoryComments.Types (DownloadCommentsRequest (DownloadCommentsRequest), HistoryCommentChunk, UploadCommentsResponse)
 import Unison.Util.Servant.CBOR
+import Unison.Util.Websockets
 
 server :: Maybe UserId -> HistoryComments.Routes WebAppServer
 server mayCaller =
@@ -26,11 +28,36 @@ downloadHistoryCommentsStreamImpl mayUserId (DownloadCommentsRequest {}) = do
   _ <- error "AUTH CHECK HERE"
   respondError Unimplemented
 
+-- Re-run the given STM action at most n times, collecting the results into a list.
+-- If the action returns Nothing, stop early and return what has been collected so far, along with a Bool indicating whether the action was exhausted.
+fetchChunk :: Int -> STM (Maybe a) -> STM ([a], Bool)
+fetchChunk size action = do
+  let go 0 = pure []
+      go n = do
+        optional action >>= \case
+          Nothing -> do
+            -- No more values available at the moment
+            pure ([], False)
+          Just Nothing -> do
+            -- Queue is closed
+            pure ([], True)
+          Just (Just val) -> do
+            (rest, exhausted) <- go (n - 1)
+            pure (val : rest, exhausted)
+  go size
+
 uploadHistoryCommentsStreamImpl :: Maybe UserId -> BranchRef -> Connection -> WebApp ()
 uploadHistoryCommentsStreamImpl mayUserId branchRef conn = do
-  _ <- error "AUTH CHECK HERE"
-  withQueues @HistoryCommentChunk @_ wsMessageBufferSize wsMessageBufferSize conn \Queues{receive, send, shutdown, connectionClosed} -> do
-    atomically receive
-  inputConduit :: ConduitT Void HistoryCommentChunk IO () <- Streaming.cborStreamToConduit inputStream
-  insertHistoryComments
-  _
+  authZ <- error "AUTH CHECK HERE"
+  projectId <- error "Process Branch Ref"
+  withQueues @HistoryCommentChunk @_ wsMessageBufferSize wsMessageBufferSize conn \Queues {receive} -> do
+    errVar <- newEmptyMVar
+    let loop = do
+          chunk <- fetchChunk insertCommentBatchSize do
+            receive <&> fmap \case
+                  HistoryCommentErrorChunk err -> Just (Left err)
+                  chunk -> Just (Right chunk)
+          PG.runTransaction $ Q.insertHistoryComments authZ projectId chunk
+   loop
+    where
+      insertCommentBatchSize = 100
